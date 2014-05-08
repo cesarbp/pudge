f8ee4dbcb0deed5bdfd2e3e7044d03801570d7ff
Cesar BP <cesar.bolanosportilla@gmail.com>
2014-05-08 17:52:13 -0500

    Fix silly

--- src/pudge/core.clj
+++ src/pudge/core.clj
@@ -9,8 +9,10 @@
 (declare make-handler)
 
 (defn start-repl!
-  "Start an http server to serve your html file on the
-specified port.
+  "Turns your clojure REPL into a cljs REPL. Also starts a local webserver that
+serves an specified html file on the root page and serves static files on every
+other page. The html file and the static files must be in the classpath.
+
 Takes a map of options. Which defaults to:
  {
   :port 8000
@@ -18,7 +20,7 @@ Takes a map of options. Which defaults to:
   :script-key :connection-script
   }
 - port is the server port.
-- html-path must be a string that points to an html in your classpath.
+- html-path must be a string that points to an html file in your classpath.
 - script-key is a keyword that must be present as a template placeholder
   in the html file, according to the library Selmer's rules. This will be used
   to insert the '<script>' tag with the required javascript for the browser
@@ -36,8 +38,8 @@ For a full example of an index.html file refer to the README.
         app (-> (make-handler html-path script-key
                               (cemerick.austin.repls/browser-connected-repl-js))
                 (middleware/wrap-content-type))]
-    {:repl (cemerick.austin.repl/cljs-repl repl-env)
-     :server (http/run-server app {:port port})}))
+    (http/run-server app {:port port})
+    (cemerick.austin.repl/cljs-repl repl-env)))
 
 (defn make-handler
   "Serve the html file on root. Everything else is treated as an static file.
